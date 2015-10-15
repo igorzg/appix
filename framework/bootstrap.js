b868376825b4dfd95d1b9e82e480bf46265c1903
igorzg <igor.zg1987@gmail.com>
2015-10-15 22:08:20 +0200

    Updated bootstrap comments

--- framework/bootstrap.js
+++ framework/bootstrap.js
@@ -3,7 +3,16 @@
 let di = require('./di');
 let Type = di.load('typed-js');
 let Component = di.load('@{en}/component');
-
+/**
+ * @license Mit Licence 2015
+ * @since 0.1.0
+ * @author Igor Ivanovic
+ * @name Bootstrap
+ *
+ * @constructor
+ * @description
+ * This class is used for component delivery service and bootstraping application
+ */
 class Bootstrap extends Type {
     constructor(config) {
         super({
@@ -20,9 +29,27 @@ class Bootstrap extends Type {
         this.listenHost = config.listenHost;
         this.component = new Component();
     }
+    /**
+     * @since 0.0.1
+     * @author Igor Ivanovic
+     * @function
+     * @name Bootstrap#setComponent
+     *
+     * @description
+     * Set component
+     */
     setComponent(key, val) {
         return this.component.set(key, val);
     }
+    /**
+     * @since 0.0.1
+     * @author Igor Ivanovic
+     * @function
+     * @name Bootstrap#getComponent
+     *
+     * @description
+     * Get component
+     */
     getComponent(key) {
         return this.component.get(key);
     }
