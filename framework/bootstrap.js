0b497f85cff142fdb04f623102e224221b49571a
igorzg <igor.zg1987@gmail.com>
2015-10-16 00:43:43 +0200

    Update of bootstrap logic, added component constants

--- framework/bootstrap.js
+++ framework/bootstrap.js
@@ -5,6 +5,12 @@ let Type = di.load('typed-js');
 let Component = di.load('@{en}/component');
 let fs = di.load('fs');
 let path = di.load('path');
+const COMPONENTS = [
+    'en/logger',
+    'en/router',
+    'en/server',
+    'en/request'
+];
 /**
  * @license Mit Licence 2015
  * @since 1.0.0
@@ -18,8 +24,10 @@ let path = di.load('path');
 class Bootstrap extends Type {
     constructor() {
         super({
-            component: Type.OBJECT
+            component: Type.OBJECT,
+            components: Type.ARRAY
         });
+        this.components = [];
         this.component = new Component();
     }
 
@@ -38,7 +46,7 @@ class Bootstrap extends Type {
             throw new TypeError('Config must be object type');
         }
 
-        let config = Object.assign({
+        let defaults = Object.assign({
             appPath: null,
             envFile: 'env.json',
             envPath: null,
@@ -48,18 +56,73 @@ class Bootstrap extends Type {
             modulesPath: '@{appPath}/modules/'
         }, appConfig);
 
-        if (!Type.isString(config.appPath)) {
+        if (!Type.isString(defaults.appPath)) {
             throw new TypeError('appPath must be defined in configuration object');
         }
 
-        di.setAlias('appPath', config.appPath);
+        di.setAlias('appPath', defaults.appPath);
 
-        if (!!config.envPath) {
-            di.setAlias('envPath', config.envPath);
+        if (!!defaults.envPath) {
+            di.setAlias('envPath', defaults.envPath);
         } else {
-            di.setAlias('envPath', config.appPath);
+            di.setAlias('envPath', defaults.appPath);
         }
 
+        // load file
+        let fileBuffer;
+        let envFile = di.normalize('@{envPath}/' + defaults.envFile);
+        try {
+            fileBuffer = fs.readFileSync(envFile);
+        } catch (e) {
+            let error = new Error('Problem with loading file, do you have your environment file json in path: ' + envFile);
+            error.stack = e.stack;
+            throw error;
+        }
+        // parse file
+        let config;
+        try {
+            config = JSON.parse(fileBuffer.toString());
+        } catch (e) {
+            let error = new Error('Problem with parsing environment json file: ' + fileBuffer.toString());
+            error.stack = e.stack;
+            throw error;
+        }
+
+        if (Type.isArray(config)) {
+            throw new TypeError('environment must be object type');
+        }
+        // set aliases
+        if (Type.isArray(config.aliases)) {
+            config.aliases.forEach(item => {
+                if (Type.isString(item.key) && Type.isString(item.value)) {
+                    di.setAlias(item.key, item.value);
+                }
+            });
+        }
+
+        if (!di.hasAlias('controllersPath')) {
+            di.setAlias('controllersPath', defaults.controllersPath);
+        }
+
+        if (!di.hasAlias('modulesPath')) {
+            di.setAlias('modulesPath', defaults.modulesPath);
+        }
+
+        if (Type.isArray(config.components)) {
+            config.components.forEach(item => {
+                if (Type.isString(item.name) && !this.hasComponent(item.name)) {
+                    let component;
+                    if (Type.isString(item.filePath)) {
+                        component = di.load(item.filePath);
+                    } else if (COMPONENTS.indexOf(item.name) > -1) {
+                        component = di.load('@{en}/' + item.name.slice(3));
+                    } else {
+                        component = di.load(item.name);
+                    }
+                    this.components.push(component);
+                }
+            });
+        }
 
     }
 
@@ -74,6 +137,19 @@ class Bootstrap extends Type {
      */
     setComponent(key, val) {
         return this.component.set(key, val);
+    }
+
+    /**
+     * @since 1.0.0
+     * @author Igor Ivanovic
+     * @function
+     * @name Bootstrap#hasComponent
+     *
+     * @description
+     * Check if has component
+     */
+    hasComponent(key) {
+        return this.component.has(key);
     }
 
     /**
