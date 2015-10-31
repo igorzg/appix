042d9a448829d80bfa6d616260ab238a0b209a6e
igorzg <igor.zg1987@gmail.com>
2015-10-31 16:52:08 +0100

    Added docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var exit = require('gulp-exit');
+var rename = require("gulp-rename");
+var gulpJsdoc2md = require("gulp-jsdoc-to-markdown");
 
-gulp.task('test', function() {
+gulp.task('test', function () {
     gulp.src(['./tests/**/*-spec.js'])
         .pipe(jasmine({
             verbose: true,
@@ -10,4 +12,18 @@ gulp.task('test', function() {
             includeStackTrace: true
         }))
         .pipe(exit());
+});
+
+
+gulp.task('docs', function () {
+    gulp.src(['./framework/**/*.js'])
+        .pipe(gulpJsdoc2md())
+        .on("error", function (err) {
+            gutil.log(gutil.colors.red("jsdoc2md failed"), err.message)
+        })
+        .pipe(rename(function(path){
+            path.extname = ".md";
+        }))
+
+        .pipe(gulp.dest("docs"));
 });
